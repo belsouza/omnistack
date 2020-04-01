bc7eb9c04fa899bbcd59491999375368bcd15c9a
Izabel Souza <belsanpesou@yahoo.com.br>
2020-04-01 12:18:54 -0300

    Inserindo e Exibindo Ongs (Json)

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    const body = request.body;
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
+    return response.json(ongs);
+});
 
-    console.log(body);
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');
 
-    return response.json({
-        evento: 'Semana Omnistack 11.0',
-        aluno: 'Cleiton Souza'
-    });
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+    //console.log(data);
+
+    return response.json({id});
 });
 
 module.exports = routes;
